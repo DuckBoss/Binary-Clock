29729b4ef1e485e6c7692a0ad91a14668b296998
Jason <DuckBoss@users.noreply.github.com>
2017-05-22 04:50:39 -0400

    Create BinaryClock.java

---
+++ BinaryClock.java
@@ -0,0 +1,21 @@
+import java.time.LocalTime;
+
+public class BinaryClock {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		while(true) {
+			LocalTime time = LocalTime.now();
+			int curSec = time.getSecond();
+			int curMin = time.getMinute();
+			int curHour = time.getMinute();
+			
+			String timeFormat = String.format("%s - %s - %s", Integer.toBinaryString(curHour), Integer.toBinaryString(curMin), Integer.toBinaryString(curSec));
+			System.out.println(timeFormat);
+			Thread.sleep(1000);
+		}
+		
+		
+	}
+
+}
