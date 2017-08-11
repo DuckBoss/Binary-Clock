3daa8380b3d8ae00f68a8ffea61f0f85fc769928
Jason <DuckBoss@users.noreply.github.com>
2017-08-11 03:35:42 -0400

    Update BinaryClock.py

--- BinaryClock.py
+++ BinaryClock.py
@@ -3,8 +3,8 @@ import datetime as dt
 
 while(True):
     curTime = dt.datetime.now()
-    curSecond = "{0:b}.format(curTime.second)
-    curMinute = "{0:b}.format(curTime.minute)
+    curSecond = "{0:b}".format(curTime.second)
+    curMinute = "{0:b}".format(curTime.minute)
     curHour = "{0:b}".format(curTime.hour)
     
     print ("{} - {} - {}".format(curHour, curMinute, curSecond))
